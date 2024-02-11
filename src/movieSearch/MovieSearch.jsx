de59cc7875762a604e1594bf5c38bbbfd72b4aa3
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2024-02-11 12:52:53 +0100

    several bug fix, loading anim

--- src/movieSearch/MovieSearch.jsx
+++ src/movieSearch/MovieSearch.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
-
+import "./MovieSearchComponent.scss"
 const MovieSearchComponent = () => {
   const [movieTitle, setMovieTitle] = useState('');
-  const [movieData, setMovieData] = useState([]);
+  const [movieData, setMovieData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`http://localhost:3000/search/byTitle/${encodeURIComponent(movieTitle)}`);
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-
-      const data = await response.json();
-      console.log('Fetched data:', data);
-      setMovieData(prevData => [...prevData, data]); // Append new data to existing data array
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-    } finally {
-      setLoading(false);
-    }
+  const cleanMovieTitle = (title) => {
+    // Remove leading and trailing spaces
+    title = title.trim();
+    // Replace multiple spaces with a single space
+    title = title.replace(/\s+/g, ' ');
+    return title;
   };
+
+  const fetchData = async () => {
+  setLoading(true);
+  try {
+    const cleanedTitle = cleanMovieTitle(movieTitle);
+
+    // Introduce a delay of 2 seconds using setTimeout
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    const response = await fetch(`http://localhost:3000/search/byTitle/${encodeURIComponent(cleanedTitle)}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    console.log('Fetched data:', data);
+
+    // Check if the response has "Search" property which indicates it's a valid movie response
+    if (data && data.Search) {
+      setMovieData(data.Search); // Set movieData to the array of movies
+    } else {
+      throw new Error('Invalid response data');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    setMovieData(null); // Clear movieData on error
+  } finally {
+    setLoading(false);
+  }
+};
+
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
@@ -43,23 +64,14 @@ const MovieSearchComponent = () => {
         />
       </label>
       {loading ? (
-        <p>Loading...</p>
-      ) : movieData.length > 0 ? (
-        movieData.map((response, index) => (
+        <div className="loading-animation"></div> // Loading animation
+      ) : movieData ? (
+        movieData.map((movie, index) => (
           <div key={index}>
-            <h2>Response {index + 1}</h2>
-            {response.Response === "True" ? (
-              response.Search.map((movie, movieIndex) => (
-                <div key={movie.imdbID}>
-                  <h3>{movie.Title}</h3>
-                  <p>Year: {movie.Year}</p>
-                  <p>Type: {movie.Type}</p>
-                  <img src={movie.Poster} alt={movie.Title}></img>
-                </div>
-              ))
-            ) : (
-              <p>No data found</p>
-            )}
+            <h2>{movie.Title}</h2>
+            <p>Year: {movie.Year}</p>
+            <p>Type: {movie.Type}</p>
+            <img src={movie.Poster} alt={movie.Title} />
           </div>
         ))
       ) : (
